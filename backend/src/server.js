f901afde7463833248067206ddcb160252415896
Sharad <sharad2014t@gmail.com>
2026-01-07 20:53:19 +0530

    fix: express 5 SPA fallback using middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.get("/{*any}", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
