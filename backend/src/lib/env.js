6c5e8665c74262b35897f2fe1f3ab4bae9cadee1
Sharad <sharad2014t@gmail.com>
2026-01-07 20:14:47 +0530

    Deploying

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -3,5 +3,13 @@ dotenv.config();
 
 export const ENV ={
     PORT: process.env.PORT,
-    MONGO_URL: process.env.MONGO_URL
+    MONGO_URL: process.env.MONGO_URL,
+    NODE_ENV: process.env.NODE_ENV,
+    // INGEST_EVENT_KEY: process.env.INGEST_EVENT_KEY,
+    // INGEST_SIGNING_KEY: process.env.INGEST_SIGNING_KEY,
+    // STREAM_API_KEY: process.env.STREAM_API_KEY,
+    // STREAM_SECRET_KEY: process.env.STREAM_SECRET_KEY,
+    // CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
+    // CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    // CLIENT_URL: process.env.CLIENT_URL,
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,32 @@
 import express from 'express';
 import { ENV } from './lib/env.js';
 import { connectDB } from './lib/db.js';
+// import { inngest, functions } from "./lib/inngest.js";
+import cors from 'cors';
+import path from 'path';
+// import { serve } from 'inngest/express';
 
 const app = express();
+const __dirname = path.resolve();
 
-app.get('/', (req, res) => {
-    res.status(200).json({msg:"Server is running successfully"});
-})
+//middleware
+app.use(express.json());
+// app.use(cors({
+//     origin: ENV.CLIENT_URL,
+//     credentials: true,  //credentials allows cookies to be sent along with requests
+// }));
+// app.use("/api/inngest", serve({ client: inngest, functions }));
+
+
+//deployment part
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 
 const startserver=async () => {
     try{
